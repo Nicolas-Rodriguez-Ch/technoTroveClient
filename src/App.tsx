92ea3da302abc3fa4c5f4c0576e5b02269284c09
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-07-18 23:01:23 -0500

    task: create folders for generic structure (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/NotFound/NotFound";
 
-import './App.css'
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
-
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <main className="App">
+      <RouterProvider router={router} />
+    </main>
+  );
 }
 
-export default App
+export default App;
