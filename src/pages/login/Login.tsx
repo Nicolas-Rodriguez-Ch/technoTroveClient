2ca93504b40979a827a4702c0c22650f4e414a16
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-07-22 15:46:36 -0500

    refactors the login view

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../../store/store";
 import { loginUser } from "../../store/reducers/users/userSlice";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router";
 import texts from "../../utils/texts";
 import { token as tknCookie } from "../../constants/cookies";
@@ -10,6 +10,8 @@ import { ToastContainer, toast } from "react-toastify";
 import Cookies from "js-cookie";
 import "react-toastify/dist/ReactToastify.css";
 import routePaths from "../../constants/routePaths";
+import { LoginResponse } from "../../store/reducers/users/userInterfaces";
+import LoginForm from "../../components/loginForm/LoginForm";
 
 interface Credentials {
   email: string;
@@ -18,49 +20,55 @@ interface Credentials {
 
 const Login = () => {
   const dispatch: AppDispatch = useDispatch();
-  const { data, status, error } = useSelector((state: RootState) => state.user);
-
-  const navigate = useNavigate();
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Credentials>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+    data: loginResponse,
+    status,
+    error,
+  } = useSelector((state: RootState) => state.user) as {
+    data: LoginResponse | null;
+    status: string;
+    error: string | null;
+  };
+  const navigate = useNavigate();
   const onSubmit: SubmitHandler<Credentials> = (data) => {
     dispatch(loginUser(data));
   };
 
-  useEffect(() => {
+  const handleSuccessfulLogin = (
+    status: string,
+    data: LoginResponse | null,
+    navigate: (path: string) => void
+  ) => {
     if (status === "succeeded" && data) {
-      if ("token" in data) {
-        const { token, data: userData } = data;
-        Cookies.set(tknCookie, token);
-        toast.success(
-          `Welcome back ${userData.fullName}, you'll soon be redirected to the Home page`,
-          {
-            position: toast.POSITION.TOP_RIGHT,
-            autoClose: 3000,
-          }
-        );
+      const { token, data: userData } = data;
+      Cookies.set(tknCookie, token);
+      toast.success(
+        `Welcome back ${userData.fullName}, you'll soon be redirected to the Home page`,
+        {
+          position: toast.POSITION.TOP_RIGHT,
+        }
+      );
 
-        setTimeout(() => {
-          navigate(routePaths.home);
-        }, 3500);
-      }
+      setTimeout(() => {
+        navigate(routePaths.home);
+      }, 5750);
     }
-  }, [status, data, navigate]);
+  };
 
-  useEffect(() => {
+  const handleFailedLogin = (status: string, error: string | null) => {
     if (status === "failed" && error) {
       toast.error(`Log in failed: ${error}`, {
         position: toast.POSITION.TOP_RIGHT,
       });
     }
+  };
+
+  useEffect(() => {
+    handleSuccessfulLogin(status, loginResponse, navigate);
+  }, [status, loginResponse, navigate]);
+
+  useEffect(() => {
+    handleFailedLogin(status, error);
   }, [status, error]);
 
   return (
@@ -68,35 +76,7 @@ const Login = () => {
       <ToastContainer />
       <section className="flex flex-col items-center bg-custom-black text-custom-mint p-4 gap-1">
         <h1 className="text-3xl font-bold">{texts.login}</h1>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col text-rigth gap-3"
-        >
-          <label htmlFor="email">{texts.email}</label>
-          <input
-            className="p-1 text-custom-black"
-            {...register("email", { required: texts.loginError })}
-            type="email"
-            placeholder={texts.email}
-            id="email"
-          />
-          <p>{errors.email?.message}</p>
-          <label htmlFor="password">{texts.password}</label>
-          <input
-            className="p-1 text-custom-black"
-            {...register("password", { required: texts.loginError })}
-            type="password"
-            placeholder={texts.password}
-            id="password"
-          />
-          <p>{errors.email?.message}</p>
-          <button
-            type="submit"
-            className="bg-custom-blue text-custom-mint border hover:bg-custom-mint hover:text-custom-blue p-1 font-bold"
-          >
-            {texts.submit}
-          </button>
-        </form>
+        <LoginForm onSubmit={onSubmit} />
       </section>
     </>
   );
