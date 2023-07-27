236dd0e4dc5f37c9c9e02c998743a180bcb836f0
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2023-07-26 22:28:05 -0500

    Feature/all users view (#9)
    * starts the page for all the users
    
    * creates the all users page and styles it
    
    * adds more styles to the components and makes them a link

---
+++ src/pages/allUsers/AllUsers.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../services/userAPI";
+import UserPreview from "../../components/userPreview/UserPreview";
+import { Link } from "react-router-dom";
+interface User {
+  id: string;
+  fullName: string;
+  email: string;
+  profilePicture?: string;
+  description: string;
+}
+
+const AllUsers = () => {
+  const [users, setUsers] = useState<Record<string, User>>({});
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await getAllUsers();
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <main className="bg-custom-black p-4 flex flex-col gap-4 items-center">
+        <h1>Supp</h1>
+        {Object.values(users).map((user) => (
+          <Link key={user.id} to={`user/${user.id}`}>
+            <UserPreview
+              fullName={user.fullName}
+              email={user.email}
+              profilePicture={user.profilePicture}
+              description={user.description}
+            />
+          </Link>
+        ))}
+      </main>
+    </>
+  );
+};
+
+export default AllUsers;
