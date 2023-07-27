cd56137c27bc338f6a23355c60614bb254685711
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-07-27 11:22:40 -0500

    starts with the creation of the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import "./Header.css";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { banner } from "../../assets/images";
 
 const Header = () => {
-  return <div className="Header">Header</div>;
+  return (
+    <main className="bg-custom-blue text-custom-mint p-4 flex justify-between">
+      <section className="flex gap-1 md:gap-4 items-center">
+        <img
+          className="w-[100px] md:w-[150px]"
+          src={banner}
+          alt="Techno Trove"
+        />
+        <h1 className="text-lg md:text-3xl">Techno Trove</h1>
+      </section>
+      <section>
+        <GiHamburgerMenu />
+      </section>
+    </main>
+  );
 };
 
 export default Header;
