0c260a84650c7fcbc95a081e183159ce2a34e7c6
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-08-01 23:57:45 -0500

    starts the handle submit for the form

--- src/components/projectForm/ProjectForm.tsx
+++ src/components/projectForm/ProjectForm.tsx
@@ -1,17 +1,19 @@
 import {
   ProjectField,
   ProjectForm as ProjectFormType,
+  ProjectImageField,
 } from "../../types/formInterfaces";
+import { useMatch } from "react-router-dom";
 import { useForm, useFieldArray } from "react-hook-form";
 import InputField from "../inputField/InputField";
 import texts from "../../utils/texts";
+import routePaths from "../../constants/routePaths";
 
 interface ProjectFormProps {
   defaultValues: ProjectFormType;
   onSubmit: (data: ProjectFormType) => Promise<void>;
   disabled: boolean;
 }
-
 const ProjectForm = ({
   defaultValues,
   onSubmit,
@@ -30,6 +32,7 @@ const ProjectForm = ({
       })) as unknown as ProjectField[],
     },
   });
+  const isEditMode = Boolean(useMatch(`${routePaths.editProject}/:id`));
 
   const { fields, append, remove } = useFieldArray({
     control,
@@ -97,7 +100,59 @@ const ProjectForm = ({
             errors={errors.description}
           />
         </div>
-
+        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4 md:mb-0">
+          {defaultValues.images &&
+            defaultValues.images.map((image, index) => (
+              <div key={index} className="w-full">
+                <img
+                  src={image as unknown as string}
+                  alt={`Previously uploaded image #${index + 1}`}
+                  className="mb-4"
+                />
+              </div>
+            ))}
+          {isEditMode &&
+            imageFields.map((item, index) => (
+              <div key={item.id} className="w-full">
+                <InputField
+                  register={register}
+                  id={`images[${index}].imageField`}
+                  label={`${texts.newProjectImage} #${index + 1}`}
+                  disabled={disabled}
+                  type="file"
+                  placeHolder={texts.newProjectImagePlaceHolder}
+                  rules={{
+                    required: false,
+                  }}
+                  defaultValue={
+                    defaultValues.images[index] as unknown as string
+                  }
+                  errors={errors.images && errors.images[index]?.imageField}
+                  accept=".png, .jpg, .jpeg"
+                />
+                {index === imageFields.length - 1 && (
+                  <div className="flex justify-center">
+                    <button
+                      type="button"
+                      className={`${BUTTON_CLASSNAME}`}
+                      onClick={() => appendImage({ imageField: "" })}
+                    >
+                      {texts.newProjectImageAdd}
+                    </button>
+                    {imageFields.length > 1 && (
+                      <button
+                        type="button"
+                        className={`${BUTTON_CLASSNAME}`}
+                        onClick={() => removeImage(index)}
+                      >
+                        {texts.remove}
+                      </button>
+                    )}
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
         <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4 md:mb-0">
           {fields.map((item, index) => (
             <div key={item.id} className="w-full">
@@ -118,14 +173,16 @@ const ProjectForm = ({
               />
               {index === fields.length - 1 && (
                 <div className="flex justify-center">
-                  <button
-                    type="button"
-                    className={`${BUTTON_CLASSNAME}`}
-                    onClick={() => append({ field: "" })}
-                  >
-                    {texts.newProjectAddLink}
-                  </button>
-                  {fields.length > 1 && (
+                  {isEditMode && (
+                    <button
+                      type="button"
+                      className={`${BUTTON_CLASSNAME}`}
+                      onClick={() => append({ field: "" })}
+                    >
+                      {texts.newProjectAddLink}
+                    </button>
+                  )}
+                  {isEditMode && fields.length > 1 && (
                     <button
                       type="button"
                       className={`${BUTTON_CLASSNAME}`}
@@ -139,58 +196,11 @@ const ProjectForm = ({
             </div>
           ))}
         </div>
-        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4 md:mb-0">
-          {defaultValues.images &&
-            defaultValues.images.map((image, index) => (
-              <div key={index} className="w-full">
-                <img
-                  src={image as unknown as string}
-                  alt={`Previously uploaded image #${index + 1}`}
-                  className="mb-4"
-                />
-              </div>
-            ))}
-          {imageFields.map((item, index) => (
-            <div key={item.id} className="w-full">
-              <InputField
-                register={register}
-                id={`images[${index}].imageField`}
-                label={`${texts.newProjectImage} #${index + 1}`}
-                disabled={disabled}
-                type="file"
-                placeHolder={texts.newProjectImagePlaceHolder}
-                rules={{
-                  required: {
-                    value: true,
-                    message: texts.newProjectImageError,
-                  },
-                }}
-                errors={errors.images && errors.images[index]?.imageField}
-                accept=".png, .jpg, .jpeg"
-              />
-              {index === imageFields.length - 1 && (
-                <div className="flex justify-center">
-                  <button
-                    type="button"
-                    className={`${BUTTON_CLASSNAME}`}
-                    onClick={() => appendImage({ imageField: "" })}
-                  >
-                    {texts.newProjectImageAdd}
-                  </button>
-                  {imageFields.length > 1 && (
-                    <button
-                      type="button"
-                      className={`${BUTTON_CLASSNAME}`}
-                      onClick={() => removeImage(index)}
-                    >
-                      {texts.remove}
-                    </button>
-                  )}
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
+        {isEditMode && (
+          <button type="submit" className={`${BUTTON_CLASSNAME}`}>
+            {texts.submit}
+          </button>
+        )}
       </form>
     </>
   );
